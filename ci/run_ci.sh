0a0d1798b3715ae87b3cb08e8b00050c17be79fb
Myungchul Keum <8174871+dofuuz@users.noreply.github.com>
2025-07-03 14:25:27 +0900

    8

--- ci/run_ci.sh
+++ ci/run_ci.sh
@@ -81,7 +81,7 @@ install_bazel() {
   BINARY_URL="https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-${OS}-${ARCH}"
 
   echo "Downloading bazel from: $BINARY_URL"
-  sudo wget -q -O "$BAZEL_DIR/bazel" "$BINARY_URL" || { echo "Failed to download bazel"; exit 1; }
+  sudo wget -q -O "$BAZEL_DIR/bazel" "$BINARY_URL" #|| { echo "Failed to download bazel"; exit 1; }
 
   sudo chmod +x "$BAZEL_DIR/bazel"
 
