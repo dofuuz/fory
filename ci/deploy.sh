783f63e171bbf130107d1df39756c791e537a387
Myungchul Keum <8174871+dofuuz@users.noreply.github.com>
2025-07-06 10:09:06 +0900

    clean-up 2

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -129,6 +129,9 @@ build_pyfory() {
   # Fix strange installed deps not found
   pip install setuptools -U
 
+  # Export currnet Python path to Bazel
+  export PYTHON_BIN_PATH=$(which python)
+
   # Detect host architecture and only pass x86_64 config when appropriate
   ARCH=$(uname -m)
   if [[ "$ARCH" == "x86_64" || "$ARCH" == "amd64" ]]; then
